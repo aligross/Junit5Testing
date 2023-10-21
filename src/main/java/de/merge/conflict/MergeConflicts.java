8a5a8fba245da68de75624c35f642d6c679b6b41
aligross <agross@agmail.de>
2023-10-21 13:27:53 +0200

    Initialcomit

---
+++ src/main/java/de/merge/conflict/MergeConflicts.java
@@ -0,0 +1,12 @@
+package de.merge.conflict;
+
+public class MergeConflicts {
+
+	// This line will be edited by master
+	
+	
+	// This line will be edited by feature.
+	
+	
+	//This line will be edited by both.
+}
